a78a187ebc9b6ac11fcbde1cfecbb35e7ea5702b
Rylan Arguelles <hideyarguelles@Rylan.local>
2020-04-15 21:16:40 +1000

    Add styling and navigation component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import Navigation from './components/universal/Navigation';
+import grey from '@material-ui/core/colors/grey';
+import { withStyles } from '@material-ui/styles';
 
-export default class App extends React.Component {
+const styles = (theme) => ({
+    root: {
+        height: '100vh',
+        padding: '64px',
+        backgroundColor: grey[900],
+    },
+});
+
+class App extends React.Component {
     render() {
+        const { classes } = this.props;
         return (
-            <div className='App'>
-                <header className='App-header'>
-                    <p>
-                        Edit <code>src/App.js</code> and save to reload.
-                    </p>
-                    <a
-                        className='App-link'
-                        href='https://reactjs.org'
-                        target='_blank'
-                        rel='noopener noreferrer'
-                    >
-                        Learn React
-                    </a>
-                </header>
-            </div>
+            <Grid container direction='column' className={classes.root}>
+                <Grid item>
+                    <Navigation />
+                </Grid>
+                <br></br>
+                <Grid item>Something</Grid>
+            </Grid>
         );
     }
 }
+
+export default withStyles(styles)(App);
