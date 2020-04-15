7a553eb0e54b2c64a23d11b663490e3ca365027a
Rylan Arguelles <hideyarguelles@Rylan.local>
2020-04-15 21:16:29 +1000

    Create initial navigation component

---
+++ src/components/universal/Navigation/index.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import grey from '@material-ui/core/colors/grey';
+import { withStyles } from '@material-ui/styles';
+
+const styles = (theme) => ({
+    navItem: {
+        color: grey[50],
+        fontFamily: 'Butler',
+        fontWeight: 'bold',
+        cursor: 'pointer',
+    },
+});
+
+class Navigation extends React.Component {
+    render() {
+        const { classes } = this.props;
+        return (
+            <Grid container direction='column' spacing={2}>
+                <Grid item>
+                    <Typography variant='h4' className={classes.navItem}>
+                        O. R. ARGUELLES
+                    </Typography>
+                </Grid>
+                <Grid item>
+                    <Grid container direction='row' spacing={4}>
+                        <Grid item>
+                            <Typography className={classes.navItem}>
+                                Home
+                            </Typography>
+                        </Grid>
+                        <Grid item>
+                            <Typography className={classes.navItem}>
+                                Code
+                            </Typography>
+                        </Grid>
+                        <Grid item>
+                            <Typography className={classes.navItem}>
+                                Music
+                            </Typography>
+                        </Grid>
+                        <Grid item>
+                            <Typography className={classes.navItem}>
+                                Analog Photography
+                            </Typography>
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </Grid>
+        );
+    }
+}
+
+export default withStyles(styles)(Navigation);
