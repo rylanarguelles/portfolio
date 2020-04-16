a0e2303c0431dc7e3eddfa042b5351aeb5e18172
Rylan Arguelles <hideyarguelles@Rylan.local>
2020-04-16 14:48:37 +1000

    Add provider and music store

--- src/index.js
+++ src/index.js
@@ -2,15 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'mobx-react';
+import MusicStore from './store/music_store';
 
-ReactDOM.render(
-    <React.StrictMode>
+const Root = (
+    <Provider MusicStore={MusicStore}>
         <App />
-    </React.StrictMode>,
-    document.getElementById('root'),
+    </Provider>
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+ReactDOM.render(Root, document.getElementById('root'));
 serviceWorker.unregister();
