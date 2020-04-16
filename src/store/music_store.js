49a1415eb2452afa252130a901a6cc98b1da38f6
Rylan Arguelles <hideyarguelles@Rylan.local>
2020-04-17 01:23:08 +1000

    Remove attribute and computed import

--- src/store/music_store.js
+++ src/store/music_store.js
@@ -1,15 +1,13 @@
-import { computed, decorate, observable } from 'mobx';
+import { decorate, observable } from 'mobx';
 
 export class MusicStore {
     songs = undefined;
     activeSong = undefined;
-    songPlaying = false;
 }
 
 decorate(MusicStore, {
     songs: observable,
     activeSong: observable,
-    songPlaying: observable,
 });
 
 export default new MusicStore();
