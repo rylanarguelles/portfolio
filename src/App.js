db81b865a8bf9d98b9eb5a85464f42aa1c91a2c7
Rylan Arguelles <hideyarguelles@Rylan.local>
2020-04-16 23:58:48 +1000

    Add router and routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Divider from '@material-ui/core/Divider';
-// import Home from './pages/Home';
+import Home from './pages/Home';
 import Music from './pages/Music';
 import Navigation from './components/universal/Navigation';
 import grey from '@material-ui/core/colors/grey';
 import { withStyles } from '@material-ui/styles';
+import { BrowserRouter as Router, Redirect, Route } from 'react-router-dom';
 
 const styles = (theme) => ({
     root: {
@@ -18,16 +19,19 @@ class App extends React.Component {
     render() {
         const { classes } = this.props;
         return (
-            <div className={classes.root}>
-                <Navigation />
-                <br></br>
-                <br></br>
-                <Divider />
-                <br></br>
-                <br></br>
-                {/* <Home /> */}
-                <Music />
-            </div>
+            <Router>
+                <div className={classes.root}>
+                    <Navigation />
+                    <br></br>
+                    <br></br>
+                    <Divider />
+                    <br></br>
+                    <br></br>
+                    <Route path='/home' component={Home} />
+                    <Route path='/music' component={Music} />
+                    <Redirect exact from='' to='/home' />
+                </div>
+            </Router>
         );
     }
 }
