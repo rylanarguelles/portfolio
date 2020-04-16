04078e92e95e4ca7b73b875243db128445a4f087
Rylan Arguelles <hideyarguelles@Rylan.local>
2020-04-16 15:49:07 +1000

    Create sounds table component

---
+++ src/pages/Music/components/SongDetailsDialog/components/SoundsTable/index.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import SoundRow from './components/SoundRow';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Typography from '@material-ui/core/Typography';
+import grey from '@material-ui/core/colors/grey';
+import { withStyles } from '@material-ui/styles';
+
+const styles = (theme) => ({
+    soundsTable: {
+        backgroundColor: grey[700],
+    },
+    text: {
+        color: grey[50],
+        fontFamily: 'Neue Haas Grotesk Text Pro',
+    },
+});
+
+class SoundsTable extends React.Component {
+    render() {
+        const { sounds, classes } = this.props;
+        return (
+            <Table className={classes.soundsTable}>
+                <TableHead>
+                    <TableRow>
+                        <TableCell>
+                            <Typography className={classes.text}>
+                                Instrument
+                            </Typography>
+                        </TableCell>
+                        <TableCell>
+                            <Typography className={classes.text}>
+                                Effects
+                            </Typography>
+                        </TableCell>
+                        <TableCell>
+                            <Typography className={classes.text}>
+                                Live
+                            </Typography>
+                        </TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    <SoundRow />
+                    <SoundRow />
+                    <SoundRow />
+                    <SoundRow />
+                </TableBody>
+            </Table>
+        );
+    }
+}
+
+export default withStyles(styles)(SoundsTable);
