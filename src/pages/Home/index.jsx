20034147618689da6bb622b8dbfce61f1430aab2
Rylan Arguelles <hideyarguelles@Rylan.local>
2020-04-16 11:09:50 +1000

    Create home page

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import Education from './components/Education';
+import Links from './components/Links';
+import WorkExperience from './components/WorkExperience';
+
+export default class Home extends React.Component {
+    render() {
+        return (
+            <Grid container direction='column' spacing={4}>
+                <Grid item>
+                    <Education />
+                </Grid>
+                <br></br>
+                <br></br>
+                <Grid item>
+                    <WorkExperience />
+                </Grid>
+                <br></br>
+                <br></br>
+                <Grid item>
+                    <Links />
+                </Grid>
+            </Grid>
+        );
+    }
+}
