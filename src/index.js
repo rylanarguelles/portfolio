516729aa33fb260fb661a82161ebf7f627c09eac
Rylan Arguelles <hideyarguelles@Rylan.local>
2020-04-17 01:23:46 +1000

    Add code store to provider

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from 'mobx-react';
+import CodeStore from './store/code_store';
 import MusicStore from './store/music_store';
 
 const Root = (
-    <Provider MusicStore={MusicStore}>
+    <Provider MusicStore={MusicStore} CodeStore={CodeStore}>
         <App />
     </Provider>
 );
