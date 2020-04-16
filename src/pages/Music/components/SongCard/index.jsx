d842eda8c7cdad23d4fe5ada37d1e79b04b8a6e0
Rylan Arguelles <hideyarguelles@Rylan.local>
2020-04-16 12:36:20 +1000

    Add component styling and change button to icon button

--- src/pages/Music/components/SongCard/index.jsx
+++ src/pages/Music/components/SongCard/index.jsx
@@ -4,6 +4,8 @@ import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Grid from '@material-ui/core/Grid';
+import IconButton from '@material-ui/core/IconButton';
+import PlayArrowIcon from '@material-ui/icons/PlayArrow';
 import Typography from '@material-ui/core/Typography';
 import grey from '@material-ui/core/colors/grey';
 import { withStyles } from '@material-ui/styles';
@@ -16,6 +18,9 @@ const styles = (theme) => ({
     text: {
         color: grey[50],
         fontFamily: 'Neue Haas Grotesk Text Pro',
+    },
+    icon: {
+        color: grey[50],
     },
 });
 
@@ -36,16 +41,16 @@ class SongCard extends React.Component {
                     </Typography>
                 </CardContent>
                 <CardActions>
-                    <Grid container direction='row' spacing={2}>
+                    <Grid
+                        container
+                        direction='row'
+                        spacing={2}
+                        alignItems='center'
+                    >
                         <Grid item>
-                            <Button>
-                                <Typography
-                                    variant='overline'
-                                    className={classes.text}
-                                >
-                                    Play
-                                </Typography>
-                            </Button>
+                            <IconButton className={classes.icon}>
+                                <PlayArrowIcon />
+                            </IconButton>
                         </Grid>
                         <Grid item>
                             <Button>
