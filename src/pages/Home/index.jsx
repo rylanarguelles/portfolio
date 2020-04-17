11fb968b3a659adaff39a10b8ec5516669ac298d
Rylan Arguelles <hideyarguelles@Rylan.local>
2020-04-17 17:25:04 +1000

    Remove line breaks and add justify

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -7,22 +7,16 @@ import WorkExperience from './components/WorkExperience';
 export default class Home extends React.Component {
     render() {
         return (
-            <Grid container direction='column' spacing={4}>
+            <Grid container direction='row' spacing={4} justify='space-between'>
                 <Grid item>
                     <Education />
                 </Grid>
-                <br></br>
-                <br></br>
                 <Grid item>
                     <WorkExperience />
                 </Grid>
-                <br></br>
-                <br></br>
                 <Grid item>
                     <Links />
                 </Grid>
-                <br></br>
-                <br></br>
             </Grid>
         );
     }
