9239251529914867a73f3f0cc1d772e56f83c515
Rylan Arguelles <hideyarguelles@Rylan.local>
2020-04-18 00:44:34 +1000

    Add temporary component mount function and conditional rendering for education component

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -6,14 +6,20 @@ import Links from './components/Links';
 import WorkExperience from './components/WorkExperience';
 
 class Home extends React.Component {
+    componentDidMount() {
+        // TODO: Service for retrieving education and experience items
+    }
+
     render() {
         const { HomeStore } = this.props;
         const { educationItems, experienceItems } = HomeStore;
         return (
             <Grid container direction='row' spacing={4} justify='space-between'>
-                <Grid item>
-                    <Education educationItems={educationItems} />
-                </Grid>
+                {educationItems && (
+                    <Grid item>
+                        <Education educationItems={educationItems} />
+                    </Grid>
+                )}
                 {experienceItems && (
                     <Grid item>
                         <WorkExperience experienceItems={experienceItems} />
