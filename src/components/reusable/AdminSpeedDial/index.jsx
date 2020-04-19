7292a91a782f6af1038174e59fb7c88001039f7c
Rylan Arguelles <hideyarguelles@Rylan.local>
2020-04-19 20:04:23 +1000

    Create admin speed dial component

---
+++ src/components/reusable/AdminSpeedDial/index.jsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import AddIcon from '@material-ui/icons/Add';
+import DeleteIcon from '@material-ui/icons/Delete';
+import SpeedDial from '@material-ui/lab/SpeedDial';
+import SpeedDialAction from '@material-ui/lab/SpeedDialAction';
+import SpeedDialIcon from '@material-ui/lab/SpeedDialIcon';
+import red from '@material-ui/core/colors/red';
+import { withStyles } from '@material-ui/styles';
+
+const styles = (theme) => ({
+    dial: {
+        position: 'absolute',
+        bottom: '64px',
+        right: '64px',
+    },
+});
+
+class AdminSpeedDial extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            dialIsOpen: false,
+        };
+
+        this.toggleDial = this.toggleDial.bind(this);
+    }
+
+    toggleDial() {
+        this.setState({
+            dialIsOpen: !this.state.dialIsOpen,
+        });
+    }
+
+    render() {
+        const {
+            hidden,
+            addTitle,
+            handleAdd,
+            deleteTitle,
+            handleDelete,
+            classes,
+        } = this.props;
+        return (
+            <SpeedDial
+                ariaLabel='Admin Speed Dial'
+                className={classes.dial}
+                icon={<SpeedDialIcon />}
+                hidden={hidden}
+                direction='up'
+                open={this.state.dialIsOpen}
+                onOpen={this.toggleDial}
+                onClose={this.toggleDial}
+            >
+                <SpeedDialAction
+                    icon={<AddIcon />}
+                    tooltipTitle={addTitle}
+                    onClick={handleAdd}
+                />
+                <SpeedDialAction
+                    icon={<DeleteIcon />}
+                    tooltipTitle={deleteTitle}
+                    onClick={handleDelete}
+                />
+            </SpeedDial>
+        );
+    }
+}
+
+export default withStyles(styles)(AdminSpeedDial);
