9ed3ca850b2002075e1c2d6a72a546d051504aaf
Rylan Arguelles <hideyarguelles@Rylan.local>
2020-04-19 23:47:21 +1000

    Add admin speed dial and add dialog

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,14 +1,41 @@
 import React from 'react';
 import { inject, observer } from 'mobx-react';
+import AdminAddDialog from '../../components/reusable/AdminAddDialog';
+import AddHomeItemFields from './components/AddHomeItemFields';
 import AdminSpeedDial from '../../components/reusable/AdminSpeedDial';
 import Grid from '@material-ui/core/Grid';
 import Education from './components/Education';
 import Links from './components/Links';
 import WorkExperience from './components/WorkExperience';
+import HomeController from '../../controllers/home';
 
 class Home extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            adminDialogIsOpen: false,
+        };
+
+        this.toggleAdminDialog = this.toggleAdminDialog.bind(this);
+        this.handleAdd = this.handleAdd.bind(this);
+    }
+
     componentDidMount() {
         // TODO: Service for retrieving education and experience items
+    }
+
+    toggleAdminDialog() {
+        this.setState({
+            adminDialogIsOpen: !this.state.adminDialogIsOpen,
+        });
+        if (!this.state.adminDialogIsOpen) {
+            HomeController.resetAddForm();
+        }
+    }
+
+    handleAdd() {
+        // TODO: Add function
     }
 
     render() {
@@ -29,7 +56,17 @@ class Home extends React.Component {
                 <Grid item>
                     <Links />
                 </Grid>
-                <AdminSpeedDial />
+                <AdminSpeedDial
+                    addTitle='Add Education / Experience'
+                    handleAdd={this.toggleAdminDialog}
+                />
+                <AdminAddDialog
+                    isOpen={this.state.adminDialogIsOpen}
+                    close={this.toggleAdminDialog}
+                    handleAdd={this.handleAdd}
+                    title='Add Experience / Education'
+                    fields={<AddHomeItemFields />}
+                />
             </Grid>
         );
     }
