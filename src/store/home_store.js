3371fe13a83e806010769dd2e981ad9668016559
Rylan Arguelles <hideyarguelles@Rylan.local>
2020-04-19 23:46:34 +1000

    Add add home item form state

--- src/store/home_store.js
+++ src/store/home_store.js
@@ -1,13 +1,16 @@
 import { decorate, observable } from 'mobx';
+import addHomeItemFormState from './home/add_home_item_form';
 
 export class HomeStore {
     educationItems = undefined;
     experienceItems = undefined;
+    addHomeItemForm = addHomeItemFormState;
 }
 
 decorate(HomeStore, {
     educationItems: observable,
     experienceItems: observable,
+    addHomeItemForm: observable,
 });
 
 export default new HomeStore();
